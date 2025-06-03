e42fdbb1560dbc1fb024a09ad73785c7bdad6d80
yonghun <asqwklop12@gmail.com>
2025-06-03 21:09:05 +0900

    [Refactor] fake레파지토리는 테스트클래스 실행전에 생성한다.

--- src/test/java/kr/hhplus/be/server/dining/restaurant/application/service/query/SearchRestaurantServiceTest.java
+++ src/test/java/kr/hhplus/be/server/dining/restaurant/application/service/query/SearchRestaurantServiceTest.java
@@ -7,10 +7,21 @@ import java.util.List;
 import kr.hhplus.be.server.dining.restaurant.application.port.out.repository.query.SearchRestaurantRepository;
 import kr.hhplus.be.server.dining.restaurant.application.service.query.fake.FakeSearchRestaurantRepository;
 import kr.hhplus.be.server.dining.restaurant.model.Restaurant;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SearchRestaurantServiceTest {
+
+  private SearchRestaurantRepository fakeSearchRestaurantRepository;
+  private SearchRestaurantService searchRestaurantService;
+
+  @BeforeEach
+  void setUp() {
+    //given
+    fakeSearchRestaurantRepository = new FakeSearchRestaurantRepository();
+    searchRestaurantService = new SearchRestaurantService(fakeSearchRestaurantRepository);
+  }
 
   @Test
   @DisplayName("데이터가 아무것도 존재하지 않는 경우")
@@ -24,13 +35,8 @@ class SearchRestaurantServiceTest {
   @Test
   @DisplayName("데이터가 존재하는 경우")
   void selectSimpleRestaurantTest() {
-    //given
-    SearchRestaurantRepository fakeSearchRestaurantRepository = new FakeSearchRestaurantRepository();
-    SearchRestaurantService searchRestaurantService = new SearchRestaurantService(fakeSearchRestaurantRepository);
-
     //when
     List<Restaurant> restaurants = searchRestaurantService.execute(Restaurant.builder().build(), null);
-
     //then
     assertThat(fakeSearchRestaurantRepository.execute()).isEqualTo(restaurants);
   }
