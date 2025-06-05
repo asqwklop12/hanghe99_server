773dceff68b5a6870ce5a9cc1275cc03c53627f4
yonghun <asqwklop12@gmail.com>
2025-06-06 02:26:34 +0900

    [Refactor] GetMapping 누락

--- src/main/java/kr/hhplus/be/server/dining/restaurant/adapter/in/web/controller/query/KeywordController.java
+++ src/main/java/kr/hhplus/be/server/dining/restaurant/adapter/in/web/controller/query/KeywordController.java
@@ -3,6 +3,7 @@ package kr.hhplus.be.server.dining.restaurant.adapter.in.web.controller.query;
 import java.util.List;
 import kr.hhplus.be.server.dining.restaurant.application.port.in.usecase.query.KeywordUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class KeywordController {
   private final KeywordUseCase keywordUseCase;
 
 
+  @GetMapping
   public List<String> execute() {
     return keywordUseCase.execute();
   }
